21767a7f2999556d866856d48deb6d8c25fb9f05
jayantjan <69378221+jayantjan@users.noreply.github.com>
2020-08-08 10:37:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAKndxGVfzHxUg4Ta8or7QEv7-iP1IeWf0",
+  authDomain: "hacking-432da.firebaseapp.com",
+  databaseURL: "https://hacking-432da.firebaseio.com",
+  projectId: "hacking-432da",
+  storageBucket: "hacking-432da.appspot.com",
+  messagingSenderId: "574132059020",
+  appId: "1:574132059020:web:c9301bf1e9b300c0db34a9",
+  measurementId: "G-WP0YPQ08XM"
 };
 
      
